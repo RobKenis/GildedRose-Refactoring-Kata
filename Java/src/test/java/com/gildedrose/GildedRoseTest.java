ac7164dac4ed41375c1399d0e8094a78617fa6f9
Rob Kenis <rob.kenis@nike.com>
2020-05-30 17:05:15 +0200

    Add test for the conjured mana cake
    This fails, I know, that's the point.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,7 +12,7 @@ class GildedRoseTest {
 
     @BeforeEach
     void setUp() {
-        items = new Item[] {
+        items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20),
                 new Item("Aged Brie", 2, 0),
                 new Item("Elixir of the Mongoose", 5, 7),
@@ -22,13 +22,13 @@ class GildedRoseTest {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
     }
 
     @Test
     @Disabled("This test is stupid")
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
@@ -37,19 +37,25 @@ class GildedRoseTest {
     @Test
     void superGenericTestForAllItems() {
         final GildedRose app = new GildedRose(items);
-         app.updateQuality();
-         verifyItem(items[0], "+5 Dexterity Vest", 9, 19);
-         verifyItem(items[1], "Aged Brie", 1, 1);
-         verifyItem(items[2], "Elixir of the Mongoose", 4, 6);
-         verifyItem(items[3], "Sulfuras, Hand of Ragnaros", 0, 80);
-         verifyItem(items[4], "Sulfuras, Hand of Ragnaros", -1, 80);
-         verifyItem(items[5], "Backstage passes to a TAFKAL80ETC concert", 14, 21);
-         verifyItem(items[6], "Backstage passes to a TAFKAL80ETC concert", 9, 50);
-         verifyItem(items[7], "Backstage passes to a TAFKAL80ETC concert", 4, 50);
-//         verifyItem(items[0], "Conjured Mana Cake", 2, 4);
+        app.updateQuality();
+        verifyItem(items[0], "+5 Dexterity Vest", 9, 19);
+        verifyItem(items[1], "Aged Brie", 1, 1);
+        verifyItem(items[2], "Elixir of the Mongoose", 4, 6);
+        verifyItem(items[3], "Sulfuras, Hand of Ragnaros", 0, 80);
+        verifyItem(items[4], "Sulfuras, Hand of Ragnaros", -1, 80);
+        verifyItem(items[5], "Backstage passes to a TAFKAL80ETC concert", 14, 21);
+        verifyItem(items[6], "Backstage passes to a TAFKAL80ETC concert", 9, 50);
+        verifyItem(items[7], "Backstage passes to a TAFKAL80ETC concert", 4, 50);
     }
 
-    private void verifyItem(Item actual, String expectedName, int expectedSellIn, int expectedQuality){
+    @Test
+    void conjuredManaCake() {
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        verifyItem(items[8], "Conjured Mana Cake", 2, 4);
+    }
+
+    private void verifyItem(Item actual, String expectedName, int expectedSellIn, int expectedQuality) {
         assertEquals(expectedName, actual.name);
         assertEquals(expectedSellIn, actual.sellIn);
         assertEquals(expectedQuality, actual.quality);
