79a7452f0faaffecdae626303115704538c607a5
Rob Kenis <rob.kenis@nike.com>
2020-05-30 17:15:45 +0200

    Extract all logic to QualityUpdater
    Still somewhat mutable, but GildedRose should just do the looping and leave
    the updating to something else.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,67 +1,20 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 class GildedRose {
     //ALSO DO NOT TOUCH THIS.
     Item[] items;
+    private final QualityUpdater qualityUpdater;
 
     public GildedRose(Item[] items) {
+        qualityUpdater = new SimpleQualityUpdater();
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            updateItem(items[i]);
-        }
-    }
-
-    private void updateItem(Item item){
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = item.quality - item.quality;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
+        items = Arrays.stream(this.items)
+                .map(qualityUpdater::update)
+                .toArray(item -> new Item[items.length]);
     }
 }
---
+++ Java/src/main/java/com/gildedrose/QualityUpdater.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public interface QualityUpdater {
+
+    Item update(Item item);
+
+}
